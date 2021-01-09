66c288a9af7d2e1a956257e53bd90396b9f93478
gmftbyGMFTBY <18811371908@163.com>
2021-01-09 10:53:26 +0800

    Sat 09 Jan 2021 10:53:26 AM CST update the post train script

--- post_train/post-train.sh
+++ post_train/post-train.sh
@@ -10,6 +10,16 @@ else
     ckpt=bert-base-uncased
 fi
 
+cp -r ../ckpt/$dataset/bert-post/* ../bak/$dataset/bert-post/
+cp -r runs ../bak/$dataset/bert-post
+
+echo "[!] back up the checkpoint and logs"
+
+rm -rf ../ckpt/$dataset/bert-post/*
+rm -rf runs
+
+echo "[!] clear the checkpoint and logs"
+
 gpu_ids=(${cuda//,/ })
 CUDA_VISIBLE_DEVICES=$cuda python -m torch.distributed.launch --nproc_per_node=${#gpu_ids[@]} --master_addr 127.0.0.1 --master_port 29400 run_mlm.py \
     --model_name_or_path $ckpt \
@@ -18,4 +28,5 @@ CUDA_VISIBLE_DEVICES=$cuda python -m torch.distributed.launch --nproc_per_node=$
     --do_train \
     --do_eval \
     --output_dir ../ckpt/$dataset/bert-post \
-    --line_by_line
+    --line_by_line \
+    --save_step 10000
