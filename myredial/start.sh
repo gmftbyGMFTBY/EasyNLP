d144a628888420b083a01786b43f4386cd694175
gmftbyGMFTBY <lantiangmftby@gmail.com>
2021-06-12 12:40:57 +0800

    Sat Jun 12 12:40:57 CST 2021

--- myredial/start.sh
+++ myredial/start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 # only for jizhi platform
 
-# the cuda nums should fit with the jizhi_config.json
-./train.sh douban dual-bert 0
+# read the number of the gpu cards from the config
+gpu_num=$(cat jizhi_config.json | python -c "import sys, json; print(json.load(sys.stdin)['host_gpu_num'])")
+echo "[!] detected $gpu_num GPUs"
+
+# generate the gpus name sequence
+split=','
+str=''
+for i in $(seq 0 $(($gpu_num-1)));
+do
+    str=$str$i$split
+done
+str=${str%?}
+
+./train.sh douban dual-bert $str
