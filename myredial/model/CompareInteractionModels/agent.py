Tue Jul  6 00:55:21 CST 2021 fix the bugs of the wrong acc calculation in the bert-ft-compare-plus

myredial/model/CompareInteractionModels/agent.py
@@ -37,33 +37,32 @@ class CompareInteractionAgent(RetrievalBaseAgent):
         correct, s = 0, 0
         for idx, batch in enumerate(pbar):
             self.optimizer.zero_grad()
-            with autocast():
-                if self.args['model'] in ['bert-ft-compare']:
+            if self.args['model'] in ['bert-ft-compare']:
+                with autocast():
                     output = self.model(batch)    # [B]
                     label = batch['label']
                     loss = self.criterion(output, label.to(torch.float))
-                    # half precision
-                    self.scaler.scale(loss).backward()
-                    self.scaler.unscale_(self.optimizer)
-                    clip_grad_norm_(self.model.parameters(), self.args['grad_clip'])
-                    self.scaler.step(self.optimizer)
-                    self.scaler.update()
-                    self.scheduler.step()
+                # half precision
+                self.scaler.scale(loss).backward()
+                self.scaler.unscale_(self.optimizer)
+                clip_grad_norm_(self.model.parameters(), self.args['grad_clip'])
+                self.scaler.step(self.optimizer)
+                self.scaler.update()
+                self.scheduler.step()
 
-                    output = torch.sigmoid(output) > 0.5
-                    now_correct = torch.sum(output == label).item()
-                elif self.args['model'] in ['bert-ft-compare-plus']:
-                    # gradient accumulation
-                    label = batch['label']
-                    loss, output = self.model(batch, scaler=self.scaler, optimizer=self.optimizer)
-                    self.scaler.unscale_(self.optimizer)
-                    clip_grad_norm_(self.model.parameters(), self.args['grad_clip'])
-                    self.scaler.step(self.optimizer)
-                    self.scaler.update()
-                    self.scheduler.step()
+                output = torch.sigmoid(output) > 0.5
+                now_correct = torch.sum(output == label).item()
+            elif self.args['model'] in ['bert-ft-compare-plus']:
+                # gradient accumulation
+                loss, output, label = self.model(batch, scaler=self.scaler, optimizer=self.optimizer)
+                self.scaler.unscale_(self.optimizer)
+                clip_grad_norm_(self.model.parameters(), self.args['grad_clip'])
+                self.scaler.step(self.optimizer)
+                self.scaler.update()
+                self.scheduler.step()
 
-                    output = output.max(dim=-1)[1]
-                    now_correct = (output.cpu() == label).sum().item()
+                output = output.max(dim=-1)[1]
+                now_correct = (output.cpu() == label.cpu()).sum().item()
             correct += now_correct
             s += len(label)
             total_loss += loss.item()

myredial/model/CompareInteractionModels/bert_ft_compare_plus.py
@@ -38,6 +38,7 @@ class BERTComparePlusRetrieval(nn.Module):
 
         tloss = 0
         outputs = []
+        avg = len(inpt) // self.inner_bsz + 1
         for i in range(0, len(inpt), self.inner_bsz):
             sub_ids = pad_sequence(
                 inpt[i:i+self.inner_bsz],
@@ -57,20 +58,22 @@ class BERTComparePlusRetrieval(nn.Module):
                 sub_tids = sub_tids.cuda()
                 sub_attn_mask = sub_attn_mask.cuda()
                 sub_label = sub_label.cuda()
-
-            output = self.model(
-                input_ids=sub_ids,
-                attention_mask=sub_attn_mask,
-                token_type_ids=sub_tids,
-            )[0]    # [B, S, E]
-            logits = self.head(output[:, 0, :])    # [B, 3]
-            outputs.append(logits)
-            loss = self.criterion(logits, sub_label)
-            tloss += loss
+            # https://pytorch.org/docs/stable/notes/amp_examples.html#gradient-clipping
+            with autocast():
+                output = self.model(
+                    input_ids=sub_ids,
+                    attention_mask=sub_attn_mask,
+                    token_type_ids=sub_tids,
+                )[0]    # [B, S, E]
+                logits = self.head(output[:, 0, :])    # [B, 3]
+                loss = self.criterion(logits, sub_label)
+                loss /= avg
             # backward and gradient accumulation
             scaler.scale(loss).backward()
+            tloss += loss
+            outputs.append(logits)
         output = torch.cat(outputs)    # [B, 3]
-        return tloss, output
+        return tloss, output, label
 
     def predict(self, batch):
         inpt = batch['ids']